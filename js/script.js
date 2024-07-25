d8d070fbaf06617f7d2e9ea225ec39308d237b2f
Denise Pingiori <pingiorid@gmail.com>
2024-07-25 17:06:45 +0200

    [upd] upd for

--- js/script.js
+++ js/script.js
@@ -2,4 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const emailList = document.getElementById('emailList');
     const emailPromises = [];
 
-    
+//ciclo for per chiamare i 10 indirizzi email 
+for (let i = 0; i < 10; i++) {
+    emailPromises.push(fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response => response.json())
+        .then(data => data.response));
+}
