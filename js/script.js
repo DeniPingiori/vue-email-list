a054bd91f50f5603a899a093749315360bb565ef
Denise Pingiori <pingiorid@gmail.com>
2024-07-25 17:15:42 +0200

    [upd] to correction

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
+//richiamo evento dal dom
 document.addEventListener('DOMContentLoaded', () => {
     const emailList = document.getElementById('emailList');
     const emailPromises = [];
 
 //ciclo for per chiamare i 10 indirizzi email 
 for (let i = 0; i < 10; i++) {
+    //fetch per chiamare le 10 api
     emailPromises.push(fetch('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(response => response.json())
         .then(data => data.response));
-}
+    }
+});
